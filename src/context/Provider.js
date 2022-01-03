0dc9757bc7708d28e0dec3043b2a95b33dafbce2
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2022-01-03 11:39:04 -0300

    Criando context

---
+++ src/context/Provider.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { requestComics } from '../services';
+import Context from './Context';
+
+function Provider({ children }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getApi = async () => {
+      const comics = await requestComics();
+      const { data: { results } } = comics;
+      setData(results);
+    };
+    getApi();
+  }, []);
+
+  const context = {
+    data,
+  };
+
+  return (
+    <Context.Provider value={context}>
+      { children }
+    </Context.Provider>
+  );
+}
+
+export default Provider;
