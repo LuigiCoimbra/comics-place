4c9e9188d8097f65d86ba59445db506365a969e8
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2022-01-03 13:00:53 -0300

    Componente Header criado

---
+++ src/components/Header/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  align-items: center;
+  background: #202020;
+  color: white;
+  display: flex;
+  height: 50px;
+  justify-content: center;
+
+  h2 {
+    font-size: 18px;
+    font-family: 'Fira Mono', monospace;
+    font-weight: 500;
+  }
+`;
