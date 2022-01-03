add5d21020e37b9646fe609caa2e5bf7463c1a1c
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2022-01-03 11:45:44 -0300

    adicionando estilo global

---
+++ src/pages/Store/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import GlobalStyle from '../../styles/GlobalStyle';
+
+// import { Container } from './styles';
+
+function Store() {
+  return (
+    <div>
+      <GlobalStyle />
+      Store
+    </div>
+  );
+}
+
+export default Store;
