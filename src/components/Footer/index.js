b057f7acf1500c62775f0232f92a99a181ac8e06
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2022-01-04 09:09:01 -0300

    Criando Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Footer() {
+  return (
+    <Container>
+      <p>
+        © 2021 - Marcus Rodrigues
+      </p>
+    </Container>
+  );
+}
+
+export default Footer;
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+  background: #202020;
+  height: 60px;
+  padding: 2em 0;
+  text-align: center;
+  width: 100%;
+
+  p {
+    color: white;
+    font-size: 15px;
+    font-weight: 700;
+  }
+`;
