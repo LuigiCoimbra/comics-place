ce35e28d85f9578c839537001486dd563ad1e445
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2022-01-04 13:39:40 -0300

    Finalizando tela de detalhes

---
+++ src/pages/Details/index.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Navigation from '../../components/Navigation';
+import { requestComicId } from '../../services';
+import GlobalStyle from '../../styles/GlobalStyle';
+import { Container } from './styles';
+import Footer from '../../components/Footer';
+
+function Details() {
+  const [comicId, setComicId] = useState([]);
+  const { idComic } = useParams();
+  useEffect(() => {
+    const requestApi = async () => {
+      const item = await requestComicId(idComic);
+      const { data } = item;
+      setComicId(data.results);
+    };
+    requestApi();
+  }, [idComic]);
+
+  return (
+    <div>
+      <GlobalStyle />
+      <Navigation
+        title="cinnecta"
+        home
+      />
+      { comicId.map((comic) => (
+        <Container>
+          <section className="title-image">
+            <h1>{comic.title}</h1>
+            <img
+              src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+              alt={comic.title}
+            />
+          </section>
+          <section className="price-title">
+            {comic.prices.map(({ price }) => (
+              <h1 className="price">{`R$ ${price}`}</h1>
+            ))}
+            <div>
+              {comic.textObjects.map((value) => (
+                <p>{value.text}</p>
+              ))}
+            </div>
+            <div className="container-div">
+              <button type="button" className="add-cart">
+                Adicionar ao carrinho
+              </button>
+            </div>
+          </section>
+        </Container>
+      )) }
+      <Footer />
+    </div>
+  );
+}
+
+export default Details;
---
+++ src/pages/Details/styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  margin: 50px;
+  display: flex;
+  height: 80vh;
+
+  .title-image {
+    font-family: 'Fira Mono', monospace;
+    text-align: center;
+    img {
+      box-shadow: 0 0 1em gray;
+      margin-top: 15px;
+      width: 500px;
+      height: 500px;
+    }
+  }
+
+  .price-title {
+    font-family: 'Fira Mono', monospace;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-left: 30px;
+    text-align: center;
+
+    .price {
+      font-size: 26px;
+    }
+
+    div {
+      margin-top: 60px;
+
+      p {
+        font-size: 18px;
+      }
+    }
+
+    .add-cart {
+      border: 2px solid #555555;
+      color: black;
+      font-size: 15px;
+      font-weight: 600;
+      height: 40px;
+      width: 220px;
+      border-radius: 6px;
+      transition-duration: 0.4s;
+    }
+
+    .add-cart:hover {
+      background-color: #555555;
+      color: white;
+      cursor: pointer;
+    }
+  }
+`;
