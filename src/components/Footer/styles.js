a93e0a3eef5c8ef86e8f91fdd93eebbfba1ac980
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2022-01-06 11:53:03 -0300

    Mudando cor de fundo e da letra

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.footer`
-  background: #202020;
+  background: #E2E2E2;
   height: 60px;
   padding: 2em 0;
   text-align: center;
   width: 100%;
 
   p {
-    color: white;
+    color: #2b2b2b;
     font-size: 15px;
     font-weight: 700;
   }
